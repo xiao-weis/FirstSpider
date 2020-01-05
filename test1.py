b5a3b2c805e7a0be9ac4cdbbe179160ef8bf2fa4
weiwei <1975441709@qq.com>
2020-01-05 17:11:41 +0800

    add line abc = 1

---
+++ test1.py
@@ -0,0 +1,20 @@
+def quick_sort(i, l, r):
+    start = numbers[l]
+    end = r
+    if(start > end):
+        return
+    while(numbers[start] < numbers[i]):
+        start+1
+    while(numbers[r] > numbers[i]):
+        end-1
+    k = numbers[start]
+    numbers[start] = numbers[end]
+    numbers[end] = k
+
+    return quick_sort()
+
+if __name__ == '__main__':
+    numbers = [3, 1, 9, 7, 2, 5]
+    print(quick_sort(numbers, 0, 4))
+
+abc = 1
